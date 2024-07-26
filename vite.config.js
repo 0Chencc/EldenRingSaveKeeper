34ebd116dfd099c795be99d01c801b0d173fbb7a
0chencc <admin@hi-ourlife.com>
2024-07-26 18:28:56 +0800

    Fixing configuration file writing errors
    This is the first time I've used tauri to compile a project, so there are a few misspellings

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build:{
-    outDir:'../dist/vite'
+    outDir:'./dist/vite'
   },
   plugins: [vue()],
   resolve:{
